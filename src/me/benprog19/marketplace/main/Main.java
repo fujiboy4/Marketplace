1f545809c522a6a9e3310072042215245695cf1e
BuildTools <unconfigured@null.spigotmc.org>
2023-11-01 22:55:27 -0400

    11/1/23: Added initial code

--- src/me/benprog19/marketplace/main/Main.java
+++ src/me/benprog19/marketplace/main/Main.java
@@ -1,10 +1,25 @@
-package me.benprog19.marketplace.main;/**
+package me.benprog19.marketplace.main;
+
+import me.benprog19.marketplace.utils.ChatUtils;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
  * -=-=-=-=-=-=-=-=-=-=-=-=-=-=-
  * Created by Ben on 11/1/2023.
  * -=-=-=-=-=-=-=-=-=-=-=-=-=-=-
  */
- 
- 
-public class Main {
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getServer().getConsoleSender().sendMessage(ChatUtils.PLUGIN_PREFIX + "Starting Marketplace, please wait for plugin to load...");
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
 
 }
--- src/me/benprog19/marketplace/utils/ChatUtils.java
+++ src/me/benprog19/marketplace/utils/ChatUtils.java
@@ -1,10 +1,16 @@
-package me.benprog19.marketplace.utils;/**
+package me.benprog19.marketplace.utils;
+
+import org.bukkit.ChatColor;
+
+/**
  * -=-=-=-=-=-=-=-=-=-=-=-=-=-=-
  * Created by Ben on 11/1/2023.
  * -=-=-=-=-=-=-=-=-=-=-=-=-=-=-
  */
- 
- 
+
+
 public class ChatUtils {
+
+    public static String PLUGIN_PREFIX = ChatColor.GOLD + "" + ChatColor.BOLD + "| Marketplace | " + ChatColor.RESET;
 
 }
